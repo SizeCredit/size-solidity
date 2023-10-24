23b7308aa14c7d43bfffc4dbc8619e6f474b7775
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-10-24 00:42:30 -0300

    Move helper function to single file

---
+++ test/ExperimentsHelper.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "@solplot/Plot.sol";
+import "../src/libraries/UserLibrary.sol";
+
+abstract contract ExperimentsHelper is Test, Plot {
+    function plot(string memory filename, BorrowerStatus memory self) internal {
+        try vm.createDir("./plots", false) {} catch {}
+        try
+            vm.removeFile(string.concat("./plots/", filename, ".csv"))
+        {} catch {}
+
+        uint256 length = self.RANC.length;
+
+        // Use first row as legend
+        // Make sure the same amount of columns are included for the legend
+        vm.writeLine(
+            string.concat("./plots/", filename, ".csv"),
+            "x axis,expectedFV,unlocked,dueFV,RANC,"
+        );
+
+        // Create input csv
+        for (uint256 i; i < length; i++) {
+            int256[] memory cols = new int256[](5);
+
+            cols[0] = int256(i * 1e18);
+            cols[1] = int256(self.expectedFV[i]);
+            cols[2] = int256(self.unlocked[i]);
+            cols[3] = int256(self.dueFV[i]);
+            cols[4] = int256(self.RANC[i]);
+
+            writeRowToCSV(string.concat("./plots/", filename, ".csv"), cols);
+        }
+
+        // Create output svg with values denominated in wad
+        plot({
+            inputCsv: string.concat("./plots/", filename, ".csv"),
+            outputSvg: string.concat("./plots/", filename, ".svg"),
+            inputDecimals: 18,
+            totalColumns: 5,
+            legend: true
+        });
+    }
+}
--- test/OrderbookExperiments.t.sol
+++ test/OrderbookExperiments.t.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import "@solplot/Plot.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 import "./mocks/OrderbookMock.sol";
@@ -12,8 +11,9 @@ import "../src/libraries/UserLibrary.sol";
 import "../src/libraries/RealCollateralLibrary.sol";
 import "../src/libraries/OfferLibrary.sol";
 import "../src/libraries/ScheduleLibrary.sol";
+import "./ExperimentsHelper.sol";
 
-contract OrderbookTest is Test, Plot {
+contract OrderbookTest is Test, ExperimentsHelper {
     using EnumerableMap for EnumerableMap.UintToUintMap;
 
     OrderbookMock public orderbook;
@@ -112,43 +112,5 @@ contract OrderbookTest is Test, Plot {
         assertTrue(!orderbook.isLiquidatable(alice), "Borrower should not be liquidatable");
 
         plot("alice_2_after_pick", orderbook.getBorrowerStatus(alice));
-    }
-
-    function plot(string memory filename, BorrowerStatus memory self) private {
-        try vm.createDir("./plots", false) {} catch {}
-        try
-            vm.removeFile(string.concat("./plots/", filename, ".csv"))
-        {} catch {}
-
-        uint256 length = self.RANC.length;
-
-        // Use first row as legend
-        // Make sure the same amount of columns are included for the legend
-        vm.writeLine(
-            string.concat("./plots/", filename, ".csv"),
-            "x axis,expectedFV,unlocked,dueFV,RANC,"
-        );
-
-        // Create input csv
-        for (uint256 i; i < length; i++) {
-            int256[] memory cols = new int256[](5);
-
-            cols[0] = int256(i * 1e18);
-            cols[1] = int256(self.expectedFV[i]);
-            cols[2] = int256(self.unlocked[i]);
-            cols[3] = int256(self.dueFV[i]);
-            cols[4] = int256(self.RANC[i]);
-
-            writeRowToCSV(string.concat("./plots/", filename, ".csv"), cols);
-        }
-
-        // Create output svg with values denominated in wad
-        plot({
-            inputCsv: string.concat("./plots/", filename, ".csv"),
-            outputSvg: string.concat("./plots/", filename, ".svg"),
-            inputDecimals: 18,
-            totalColumns: 5,
-            legend: true
-        });
     }
 }
