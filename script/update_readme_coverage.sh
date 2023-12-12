46a0fbd43411829884285d601fa352e534d65224
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-12-12 18:05:07 +0100

    Update deps

---
+++ script/update_readme_coverage.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eux
+
+forge coverage --ir-minimum > COVERAGE.txt
+
+BEGIN=$(grep -n BEGIN_COVERAGE README.md | cut -d : -f 1)
+END=$(grep -n END_COVERAGE README.md | cut -d : -f 1)
+
+PART_1=$(head -n $((BEGIN)) README.md)
+PART_3=$(tail -n +$((END)) README.md)
+
+echo "$PART_1" > README.md
+cat COVERAGE.txt >> README.md
+echo "$PART_3" >> README.md
+
+rm COVERAGE.txt
