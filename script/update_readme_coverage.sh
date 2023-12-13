80c9c70597bc2d2a9eb704b3942695577f4beee3
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-12-13 11:31:10 +0100

    Update coverage script (3)

--- script/update_readme_coverage.sh
+++ script/update_readme_coverage.sh
@@ -2,12 +2,19 @@
 
 set -eu
 
+forge coverage --ir-minimum > COVERAGE.txt
+
 BEGIN=$(grep -n BEGIN_COVERAGE README.md | cut -d : -f 1)
 END=$(grep -n END_COVERAGE README.md | cut -d : -f 1)
 
 PART_1=$(head -n $((BEGIN)) README.md)
 PART_3=$(tail -n +$((END)) README.md)
 
+COVERAGE_BEGIN=$(grep -n '\bFile\b' COVERAGE.txt | cut -d : -f 1)
+COVERAGE=$(tail -n +$((COVERAGE_BEGIN)) COVERAGE.txt)
+
 echo "$PART_1" > README.md
-forge coverage --ir-minimum | grep '\|' | grep -v 'test/' | grep -v '\bTotal\b' >> README.md
+echo "$COVERAGE" | grep -v 'test/' | grep -v '\bTotal\b' >> README.md
 echo "$PART_3" >> README.md
+
+rm COVERAGE.txt
