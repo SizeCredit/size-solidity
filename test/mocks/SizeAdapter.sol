18550edca328f4a8c827706660d0c8cd866ee4b9
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-01-02 16:56:03 -0300

    Update SizeAdapter to make it easier to integrat into scaffold-eth-2

--- test/mocks/SizeAdapter.sol
+++ test/mocks/SizeAdapter.sol
@@ -22,88 +22,89 @@ import {WithdrawParams} from "@src/libraries/actions/Withdraw.sol";
 import {Size} from "@src/Size.sol";
 
 contract SizeAdapter is Size {
-// Change Size functions visibility to public for testing
-// function ___deposit(address token, uint256 amount) external {
-//     deposit(DepositParams({token: token, amount: amount}));
-// }
+/*
+    function deposit(address token, uint256 amount) external {
+        _deposit(DepositParams({token: token, amount: amount}));
+    }
 
-// function ___withdraw(address token, uint256 amount) external {
-//     withdraw(WithdrawParams({token: token, amount: amount}));
-// }
+    function withdraw(address token, uint256 amount) external {
+        _withdraw(WithdrawParams({token: token, amount: amount}));
+    }
 
-// function ___borrowAsMarketOrder(
-//     address lender,
-//     uint256 amount,
-//     uint256 dueDate,
-//     bool exactAmountIn,
-//     uint256[] memory virtualCollateralLoanIds
-// ) external {
-//     borrowAsMarketOrder(
-//         BorrowAsMarketOrderParams({
-//             lender: lender,
-//             amount: amount,
-//             dueDate: dueDate,
-//             exactAmountIn: exactAmountIn,
-//             virtualCollateralLoanIds: virtualCollateralLoanIds
-//         })
-//     );
-// }
+    function borrowAsMarketOrder(
+        address lender,
+        uint256 amount,
+        uint256 dueDate,
+        bool exactAmountIn,
+        uint256[] memory virtualCollateralLoanIds
+    ) external {
+        _borrowAsMarketOrder(
+            BorrowAsMarketOrderParams({
+                lender: lender,
+                amount: amount,
+                dueDate: dueDate,
+                exactAmountIn: exactAmountIn,
+                virtualCollateralLoanIds: virtualCollateralLoanIds
+            })
+        );
+    }
 
-// function ___borrowAsLimitOrder(uint256 maxAmount, uint256[] memory timeBuckets, uint256[] memory rates) external {
-//     borrowAsLimitOrder(
-//         BorrowAsLimitOrderParams({
-//             maxAmount: maxAmount,
-//             curveRelativeTime: YieldCurve({timeBuckets: timeBuckets, rates: rates})
-//         })
-//     );
-// }
+    function borrowAsLimitOrder(uint256 maxAmount, uint256[] memory timeBuckets, uint256[] memory rates) external {
+        _borrowAsLimitOrder(
+            BorrowAsLimitOrderParams({
+                maxAmount: maxAmount,
+                curveRelativeTime: YieldCurve({timeBuckets: timeBuckets, rates: rates})
+            })
+        );
+    }
 
-// function ___lendAsMarketOrder(address borrower, uint256 dueDate, uint256 amount, bool exactAmountIn) external {
-//     lendAsMarketOrder(
-//         LendAsMarketOrderParams({borrower: borrower, dueDate: dueDate, amount: amount, exactAmountIn: exactAmountIn})
-//     );
-// }
+    function lendAsMarketOrder(address borrower, uint256 dueDate, uint256 amount, bool exactAmountIn) external {
+        _lendAsMarketOrder(
+            LendAsMarketOrderParams({borrower: borrower, dueDate: dueDate, amount: amount, exactAmountIn: exactAmountIn})
+        );
+    }
 
-// function ___lendAsLimitOrder(
-//     uint256 maxAmount,
-//     uint256 maxDueDate,
-//     uint256[] memory timeBuckets,
-//     uint256[] memory rates
-// ) external {
-//     lendAsLimitOrder(
-//         LendAsLimitOrderParams({
-//             maxAmount: maxAmount,
-//             maxDueDate: maxDueDate,
-//             curveRelativeTime: YieldCurve({timeBuckets: timeBuckets, rates: rates})
-//         })
-//     );
-// }
+    function lendAsLimitOrder(
+        uint256 maxAmount,
+        uint256 maxDueDate,
+        uint256[] memory timeBuckets,
+        uint256[] memory rates
+    ) external {
+        _lendAsLimitOrder(
+            LendAsLimitOrderParams({
+                maxAmount: maxAmount,
+                maxDueDate: maxDueDate,
+                curveRelativeTime: YieldCurve({timeBuckets: timeBuckets, rates: rates})
+            })
+        );
+    }
 
-// function ___borrowerExit(uint256 loanId, address borrowerToExitTo) external {
-//     borrowerExit(BorrowerExitParams({loanId: loanId, borrowerToExitTo: borrowerToExitTo}));
-// }
+    function borrowerExit(uint256 loanId, address borrowerToExitTo) external {
+        _borrowerExit(BorrowerExitParams({loanId: loanId, borrowerToExitTo: borrowerToExitTo}));
+    }
 
-// function ___repay(uint256 loanId) external {
-//     repay(RepayParams({loanId: loanId}));
-// }
+    function repay(uint256 loanId) external {
+        _repay(RepayParams({loanId: loanId}));
+    }
 
-// function ___claim(uint256 loanId) external {
-//     claim(ClaimParams({loanId: loanId}));
-// }
+    function claim(uint256 loanId) external {
+        _claim(ClaimParams({loanId: loanId}));
+    }
 
-// function ___liquidateLoan(uint256 loanId) external returns (uint256) {
-//     return liquidateLoan(LiquidateLoanParams({loanId: loanId}));
-// }
+    function liquidateLoan(uint256 loanId) external returns (uint256) {
+        return _liquidateLoan(LiquidateLoanParams({loanId: loanId}));
+    }
 
-// function ___selfLiquidateLoan(uint256 loanId) external {
-//     selfLiquidateLoan(SelfLiquidateLoanParams({loanId: loanId}));
-// }
+    function selfLiquidateLoan(uint256 loanId) external {
+        _selfLiquidateLoan(SelfLiquidateLoanParams({loanId: loanId}));
+    }
 
-// function ___liquidateLoanWithReplacement(uint256 loanId, address borrower) external returns (uint256) {
-//     return liquidateLoanWithReplacement(LiquidateLoanWithReplacementParams({loanId: loanId, borrower: borrower}));
-// }
+    function liquidateLoanWithReplacement(uint256 loanId, address borrower) external returns (uint256) {
+        return _liquidateLoanWithReplacement(LiquidateLoanWithReplacementParams({loanId: loanId, borrower: borrower}));
+    }
 
-// function ___moveToVariablePool(uint256 loanId) external {
-//     moveToVariablePool(MoveToVariablePoolParams({loanId: loanId}));
-// }
+    function moveToVariablePool(uint256 loanId) external {
+        _moveToVariablePool(MoveToVariablePoolParams({loanId: loanId}));
+    }
+*/
 }
