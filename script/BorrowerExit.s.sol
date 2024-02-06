4046cffaa71c4215c694856462f0f3b51995ed50
0xn2 <0xleansan@gmail.com>
2024-02-06 12:14:54 -0300

    BorrowerExit works

--- script/BorrowerExit.s.sol
+++ script/BorrowerExit.s.sol
@@ -11,19 +11,20 @@ contract BorrowerExitScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address sizeContractAddress = vm.envAddress("SIZE_CONTRACT_ADDRESS");
 
+        address LenderTest = 0xD20baecCd9F77fAA9E2C2B185F33483D7911f9C8;
+
         address to = vm.addr(deployerPrivateKey);
 
         Size sizeContract = Size(sizeContractAddress);
 
         /// BorrowerExit struct
-        BorrowerExitParams memory params = BorrowerExitParams({loanId: 0, borrowerToExitTo: to});
+        BorrowerExitParams memory params = BorrowerExitParams({
+            loanId: 0,
+            borrowerToExitTo: LenderTest
+        });
 
         vm.startBroadcast(deployerPrivateKey);
         sizeContract.borrowerExit(params);
         vm.stopBroadcast();
     }
 }
-/* struct BorrowerExitParams {
-    uint256 loanId;
-    address borrowerToExitTo;
-} */
