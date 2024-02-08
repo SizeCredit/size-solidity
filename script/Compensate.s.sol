249c797baa9f4de695a365f916ee6b1763cb989a
0xn2 <0xleansan@gmail.com>
2024-02-08 11:39:11 -0300

    compensate method

--- script/Compensate.s.sol
+++ script/Compensate.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "../src/Size.sol";
 import "forge-std/Script.sol";
+import "../src/SizeView.sol";
 
 contract CompensateScript is Script {
     function run() external {
@@ -11,26 +12,42 @@ contract CompensateScript is Script {
 
         address LenderTest = 0xD20baecCd9F77fAA9E2C2B185F33483D7911f9C8;
         address BorrowerTest = 0x979Af411D048b453E3334C95F392012B3BbD6215;
-        uint256 amount = 1e6;
-
+      
         address currentAddress = vm.addr(deployerPrivateKey);
         Size sizeContract = Size(sizeContractAddress);
-        uint256 repaidFixedLoanDebtAfter = sizeContract.getFixedLoan(2).debt;
+        SizeView sizeViewContract = SizeView(sizeContractAddress);
+
+        console.log(currentAddress);
+
+        uint256 getcredit = sizeViewContract.getCredit(2);
+        uint256 balance = sizeViewContract
+            .getUserView(currentAddress)
+            .collateralAmount;
+        uint256 debt = sizeViewContract.getUserView(currentAddress).debtAmount;
+
+        FixedLoan[] memory loans = sizeViewContract.getFixedLoans();
+
+        for (uint i = 0; i < loans.length; i++) {
+            console.log("Loan Index:", i);
+            console.log("Lender Address:", loans[i].generic.lender);
+            console.log("Borrower Address:", loans[i].generic.borrower);
+            console.log("Credit:", loans[i].generic.credit);
+            console.log("Issuance Value:", loans[i].fol.issuanceValue);
+            console.log("Rate:", loans[i].fol.rate);
+            console.log("Start Date:", loans[i].fol.startDate);
+            console.log("Due Date:", loans[i].fol.dueDate);
+        }
+
+        console.log("balance", balance);
+        console.log("debt", debt);
+
         /// CompensateParams struct
         CompensateParams memory params = CompensateParams({
             loanToRepayId: 2,
             loanToCompensateId: 2,
-            amount: repaidFixedLoanDebtAfter
+            amount: debt
         });
 
-        console.log(currentAddress);
-
-        address compensatedFixedLoanCreditAfter = sizeContract
-            .getFixedLoan(2)
-            .lender;
-        console.log(sizeContract.getFixedLoan(2).borrower);
-        //console.log(compensatedFixedLoanCreditAfter);
-        console.log(repaidFixedLoanDebtAfter);
         vm.startBroadcast(deployerPrivateKey);
         sizeContract.compensate(params);
         vm.stopBroadcast();
