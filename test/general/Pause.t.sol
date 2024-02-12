46370e21dc0e1abd998577f5857abba9374a1f25
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-02-12 09:31:46 -0300

    Expose pause/unpause methods to PAUSER_ROLE

---
+++ test/general/Pause.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+import {DepositParams} from "@src/libraries/fixed/actions/Deposit.sol";
+import {UpdateConfigParams} from "@src/libraries/general/actions/UpdateConfig.sol";
+import {BaseTest} from "@test/BaseTest.sol";
+
+contract PauseTest is BaseTest {
+    function test_Pause_pause_cannot_deposit() public {
+        _mint(address(weth), alice, 2e18);
+
+        _deposit(alice, weth, 1e18);
+
+        size.pause();
+        vm.expectRevert(abi.encodePacked(Pausable.EnforcedPause.selector));
+        size.deposit(DepositParams({token: address(weth), amount: 1e18, to: alice}));
+    }
+
+    function test_Pause_pause_can_updateConfig() public {
+        assertGt(size.config().repayFeeAPR, 0);
+        size.pause();
+        size.updateConfig(UpdateConfigParams({key: "repayFeeAPR", value: 0}));
+        assertEq(size.config().repayFeeAPR, 0);
+    }
+}
