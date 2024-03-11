f0f5367f813a6e50b0c524226aa8a004712520a4
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-03-11 15:34:23 -0300

    Fix

--- test/BaseTestVariable.sol
+++ test/BaseTestVariable.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.24;
 import {IERC20Metadata} from "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
 import {Test} from "forge-std/Test.sol";
 
+import {BorrowVariableParams} from "@src/libraries/variable/actions/BorrowVariable.sol";
+
+import {LiquidateVariableParams} from "@src/libraries/variable/actions/LiquidateVariable.sol";
 import {RepayVariableParams} from "@src/libraries/variable/actions/RepayVariable.sol";
 
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
@@ -17,12 +20,18 @@ abstract contract BaseTestVariable is Test, BaseTestGeneral {
     }
 
     function _borrowVariable(address user, IERC20Metadata token, uint256 amount) internal {
+        if (token != usdc) revert Error.INVALID_TOKEN(address(token));
         vm.prank(user);
-        variablePool.borrow(address(token), amount, 2, 0, user);
+        size.borrowVariable(BorrowVariableParams({amount: amount, to: user}));
     }
 
     function _repayVariable(address user, uint256 amount) internal {
         vm.prank(user);
         size.repayVariable(RepayVariableParams({amount: amount}));
+    }
+
+    function _liquidateVariable(address user, address borrower, uint256 amount) internal {
+        vm.prank(user);
+        size.liquidateVariable(LiquidateVariableParams({borrower: borrower, amount: amount}));
     }
 }
