5d74eba89bd3103b6234c2f10b36c7c1467cdda2
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-03-14 11:58:13 -0300

    Fix OAK-27

--- src/proxy/Vault.sol
+++ src/proxy/Vault.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.24;
 
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
 import {Errors} from "@src/libraries/Errors.sol";
 
@@ -26,9 +27,7 @@ contract Vault is Initializable, OwnableUpgradeable {
         }
         bool success;
         (success, returnData) = address(target).call(data);
-        if (!success) {
-            revert Errors.PROXY_CALL_FAILED(target, data);
-        }
+        Address.verifyCallResult(success, returnData);
     }
 
     function proxy(address target, bytes memory data, uint256 value)
@@ -41,27 +40,27 @@ contract Vault is Initializable, OwnableUpgradeable {
         }
         bool success;
         (success, returnData) = address(target).call{value: value}(data);
-        if (!success) {
-            revert Errors.PROXY_CALL_FAILED(target, data);
-        }
+        Address.verifyCallResult(success, returnData);
     }
 
-    function proxy(address[] memory targets, bytes[] memory datas) public onlyOwner returns (bytes[] memory results) {
+    function proxy(address[] memory targets, bytes[] memory datas)
+        public
+        onlyOwner
+        returns (bytes[] memory returnDatas)
+    {
         if (targets.length != datas.length) {
             revert Errors.ARRAY_LENGTHS_MISMATCH();
         }
 
-        results = new bytes[](datas.length);
+        returnDatas = new bytes[](datas.length);
         bool success;
         for (uint256 i = 0; i < targets.length; i++) {
             if (targets[i] == address(0)) {
                 revert Errors.NULL_ADDRESS();
             }
             // slither-disable-next-line calls-loop
-            (success, results[i]) = address(targets[i]).call(datas[i]);
-            if (!success) {
-                revert Errors.PROXY_CALL_FAILED(targets[i], datas[i]);
-            }
+            (success, returnDatas[i]) = address(targets[i]).call(datas[i]);
+            Address.verifyCallResult(success, returnDatas[i]);
         }
     }
 
