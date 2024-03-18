8373e85044b069fd753b98ddbfa2814a79bc9896
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-03-18 15:02:25 -0300

    Remove unused var

--- src/libraries/fixed/AccountingLibrary.sol
+++ src/libraries/fixed/AccountingLibrary.sol
@@ -6,7 +6,7 @@ import {State} from "@src/SizeStorage.sol";
 import {ConversionLibrary} from "@src/libraries/ConversionLibrary.sol";
 
 import {Events} from "@src/libraries/Events.sol";
-import {Math, PERCENT} from "@src/libraries/Math.sol";
+import {Math} from "@src/libraries/Math.sol";
 
 import {CreditPosition, DebtPosition, LoanLibrary, RESERVED_ID} from "@src/libraries/fixed/LoanLibrary.sol";
 import {RiskLibrary} from "@src/libraries/fixed/RiskLibrary.sol";
