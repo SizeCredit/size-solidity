5d8bcc59aae9d2c5fd40b31345ff828a9227d785
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-03-20 18:00:31 -0300

    Fix PoolMock

--- test/mocks/PoolMock.sol
+++ test/mocks/PoolMock.sol
@@ -14,11 +14,9 @@ import {VariableDebtToken} from "@aave/protocol/tokenization/VariableDebtToken.s
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {EnumerableMap} from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 
 contract PoolMock is Ownable {
     using SafeERC20 for IERC20Metadata;
-    using EnumerableMap for EnumerableMap.AddressToUintMap;
 
     struct Data {
         AToken aToken;
@@ -38,24 +36,28 @@ contract PoolMock is Ownable {
         if (data.reserveIndex == 0) {
             data.aToken = new AToken(IPool(address(this)));
             data.debtToken = new VariableDebtToken(IPool(address(this)));
+            MockIncentivesController incentivesController = new MockIncentivesController();
+            uint8 decimals = IERC20Metadata(asset).decimals();
+            string memory name = IERC20Metadata(asset).name();
+            string memory symbol = IERC20Metadata(asset).symbol();
 
             data.aToken.initialize(
                 IPool(address(this)),
                 owner(),
                 asset,
-                new MockIncentivesController(),
-                IERC20Metadata(asset).decimals(),
-                string.concat("Size aToken ", IERC20Metadata(asset).name()),
+                incentivesController,
+                decimals,
+                string.concat("Size aToken ", name),
                 string.concat("asz", IERC20Metadata(asset).symbol()),
                 ""
             );
             data.debtToken.initialize(
                 IPool(address(this)),
                 asset,
-                new MockIncentivesController(),
-                IERC20Metadata(asset).decimals(),
-                string.concat("Size variableDebtToken ", IERC20Metadata(asset).name()),
-                string.concat("dsz", IERC20Metadata(asset).symbol()),
+                incentivesController,
+                decimals,
+                string.concat("Size variableDebtToken ", name),
+                string.concat("dsz", symbol),
                 ""
             );
         }
@@ -63,13 +65,13 @@ contract PoolMock is Ownable {
     }
 
     function supply(address asset, uint256 amount, address onBehalfOf, uint16) external {
-        Data storage data = datas[asset];
+        Data memory data = datas[asset];
         IERC20Metadata(asset).transferFrom(msg.sender, address(this), amount);
         data.aToken.mint(address(this), onBehalfOf, amount, data.reserveIndex);
     }
 
     function withdraw(address asset, uint256 amount, address to) external returns (uint256) {
-        Data storage data = datas[asset];
+        Data memory data = datas[asset];
         data.aToken.burn(msg.sender, address(data.aToken), amount, data.reserveIndex);
         IERC20Metadata(asset).safeTransfer(to, amount);
         return amount;
@@ -77,6 +79,10 @@ contract PoolMock is Ownable {
 
     function getReserveNormalizedIncome(address asset) external view returns (uint256) {
         return datas[asset].reserveIndex;
+    }
+
+    function getReserveData(address asset) external view returns (DataTypes.ReserveData memory reserveData) {
+        reserveData.aTokenAddress = address(datas[asset].aToken);
     }
 
     function ADDRESSES_PROVIDER() external view returns (IPoolAddressesProvider) {
