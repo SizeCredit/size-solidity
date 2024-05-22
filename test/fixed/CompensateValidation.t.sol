ee47fc23ccda48f6da478f4e4734dd270b0a1615
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-05-22 15:53:03 -0300

    Add more tests

--- test/fixed/CompensateValidation.t.sol
+++ test/fixed/CompensateValidation.t.sol
@@ -9,139 +9,137 @@ import {CompensateParams} from "@src/libraries/fixed/actions/Compensate.sol";
 import {Errors} from "@src/libraries/Errors.sol";
 
 contract CompensateValidationTest is BaseTest {
-// function test_Compensate_validation() public {
-//     _deposit(alice, weth, 100e18);
-//     _deposit(alice, usdc, 100e6);
-//     _deposit(bob, weth, 100e18);
-//     _deposit(bob, usdc, 100e6);
-//     _deposit(candy, weth, 100e18);
-//     _deposit(candy, usdc, 100e6);
-//     _deposit(james, weth, 100e18);
-//     _deposit(james, usdc, 100e6);
-//     _lendAsLimitOrder(
-//         alice, block.timestamp + 12 days, [int256(0.05e18), int256(0.05e18)], [uint256(6 days), uint256(12 days)]
-//     );
-//     _lendAsLimitOrder(
-//         bob, block.timestamp + 12 days, [int256(0.05e18), int256(0.05e18)], [uint256(6 days), uint256(12 days)]
-//     );
-//     _lendAsLimitOrder(
-//         candy, block.timestamp + 12 days, [int256(0.05e18), int256(0.05e18)], [uint256(6 days), uint256(12 days)]
-//     );
-//     _lendAsLimitOrder(
-//         james, block.timestamp + 12 days, [int256(0.05e18), int256(0.05e18)], [uint256(6 days), uint256(12 days)]
-//     );
-//     uint256 debtPositionId = _borrowAsMarketOrder(bob, alice, 20e6, block.timestamp + 12 days);
-//     uint256 creditPositionId = size.getCreditPositionIdsByDebtPositionId(debtPositionId)[0];
-//     uint256 loanId2 = _borrowAsMarketOrder(candy, bob, 20e6, block.timestamp + 12 days);
-//     uint256 creditPositionId2 = size.getCreditPositionIdsByDebtPositionId(loanId2)[0];
-//     uint256 loanId3 = _borrowAsMarketOrder(alice, james, 20e6, block.timestamp + 12 days);
-//     uint256 creditPositionId3 = size.getCreditPositionIdsByDebtPositionId(loanId3)[0];
-//     _borrowAsMarketOrder(
-//         bob, alice, 10e6, block.timestamp + 12 days, size.getCreditPositionIdsByDebtPositionId(loanId2)
-//     );
-//     uint256 creditPositionId2_1 = size.getCreditPositionIdsByDebtPositionId(loanId2)[1];
+    function test_Compensate_validation() public {
+        _deposit(alice, weth, 100e18);
+        _deposit(alice, usdc, 100e6);
+        _deposit(bob, weth, 100e18);
+        _deposit(bob, usdc, 100e6);
+        _deposit(candy, weth, 100e18);
+        _deposit(candy, usdc, 100e6);
+        _deposit(james, weth, 100e18);
+        _deposit(james, usdc, 100e6);
+        _lendAsLimitOrder(
+            alice, block.timestamp + 12 days, [int256(0.05e18), int256(0.05e18)], [uint256(6 days), uint256(12 days)]
+        );
+        _lendAsLimitOrder(
+            bob, block.timestamp + 12 days, [int256(0.05e18), int256(0.05e18)], [uint256(6 days), uint256(12 days)]
+        );
+        _lendAsLimitOrder(
+            candy, block.timestamp + 12 days, [int256(0.05e18), int256(0.05e18)], [uint256(6 days), uint256(12 days)]
+        );
+        _lendAsLimitOrder(
+            james, block.timestamp + 12 days, [int256(0.05e18), int256(0.05e18)], [uint256(6 days), uint256(12 days)]
+        );
+        uint256 debtPositionId = _borrow(bob, alice, 20e6, block.timestamp + 12 days);
+        uint256 creditPositionId = size.getCreditPositionIdsByDebtPositionId(debtPositionId)[1];
+        uint256 loanId2 = _borrow(candy, bob, 20e6, block.timestamp + 12 days);
+        uint256 creditPositionId2 = size.getCreditPositionIdsByDebtPositionId(loanId2)[1];
+        uint256 loanId3 = _borrow(alice, james, 20e6, block.timestamp + 12 days);
+        uint256 creditPositionId3 = size.getCreditPositionIdsByDebtPositionId(loanId3)[1];
+        _sellCreditMarket(bob, alice, creditPositionId2, 10e6, block.timestamp + 12 days);
+        uint256 creditPositionId2_1 = size.getCreditPositionIdsByDebtPositionId(loanId2)[1];
 
-//     vm.startPrank(bob);
-//     vm.expectRevert(abi.encodeWithSelector(Errors.COMPENSATOR_IS_NOT_BORROWER.selector, bob, alice));
-//     size.compensate(
-//         CompensateParams({
-//             creditPositionWithDebtToRepayId: creditPositionId3,
-//             creditPositionToCompensateId: creditPositionId,
-//             amount: type(uint256).max
-//         })
-//     );
-//     vm.stopPrank();
+        vm.startPrank(bob);
+        vm.expectRevert(abi.encodeWithSelector(Errors.COMPENSATOR_IS_NOT_BORROWER.selector, bob, alice));
+        size.compensate(
+            CompensateParams({
+                creditPositionWithDebtToRepayId: creditPositionId3,
+                creditPositionToCompensateId: creditPositionId,
+                amount: type(uint256).max
+            })
+        );
+        vm.stopPrank();
 
-//     vm.startPrank(alice);
-//     vm.expectRevert(abi.encodeWithSelector(Errors.INVALID_LENDER.selector, bob));
-//     size.compensate(
-//         CompensateParams({
-//             creditPositionWithDebtToRepayId: creditPositionId3,
-//             creditPositionToCompensateId: creditPositionId2,
-//             amount: type(uint256).max
-//         })
-//     );
-//     vm.stopPrank();
+        vm.startPrank(alice);
+        vm.expectRevert(abi.encodeWithSelector(Errors.INVALID_LENDER.selector, bob));
+        size.compensate(
+            CompensateParams({
+                creditPositionWithDebtToRepayId: creditPositionId3,
+                creditPositionToCompensateId: creditPositionId2,
+                amount: type(uint256).max
+            })
+        );
+        vm.stopPrank();
 
-//     vm.startPrank(alice);
-//     vm.expectRevert(abi.encodeWithSelector(Errors.NULL_AMOUNT.selector));
-//     size.compensate(
-//         CompensateParams({
-//             creditPositionWithDebtToRepayId: creditPositionId3,
-//             creditPositionToCompensateId: creditPositionId,
-//             amount: 0
-//         })
-//     );
-//     vm.stopPrank();
+        vm.startPrank(alice);
+        vm.expectRevert(abi.encodeWithSelector(Errors.NULL_AMOUNT.selector));
+        size.compensate(
+            CompensateParams({
+                creditPositionWithDebtToRepayId: creditPositionId3,
+                creditPositionToCompensateId: creditPositionId,
+                amount: 0
+            })
+        );
+        vm.stopPrank();
 
-//     _repay(bob, debtPositionId);
+        _repay(bob, debtPositionId);
 
-//     vm.startPrank(alice);
-//     uint256 cr = size.collateralRatio(bob);
-//     vm.expectRevert(
-//         abi.encodeWithSelector(
-//             Errors.CREDIT_POSITION_NOT_TRANSFERRABLE.selector, creditPositionId, LoanStatus.REPAID, cr
-//         )
-//     );
-//     size.compensate(
-//         CompensateParams({
-//             creditPositionWithDebtToRepayId: creditPositionId3,
-//             creditPositionToCompensateId: creditPositionId,
-//             amount: type(uint256).max
-//         })
-//     );
-//     vm.stopPrank();
+        vm.startPrank(alice);
+        uint256 cr = size.collateralRatio(bob);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Errors.CREDIT_POSITION_NOT_TRANSFERRABLE.selector, creditPositionId, LoanStatus.REPAID, cr
+            )
+        );
+        size.compensate(
+            CompensateParams({
+                creditPositionWithDebtToRepayId: creditPositionId3,
+                creditPositionToCompensateId: creditPositionId,
+                amount: type(uint256).max
+            })
+        );
+        vm.stopPrank();
 
-//     _repay(alice, loanId3);
+        _repay(alice, loanId3);
 
-//     vm.startPrank(alice);
-//     vm.expectRevert(abi.encodeWithSelector(Errors.LOAN_NOT_ACTIVE.selector, creditPositionId3));
-//     size.compensate(
-//         CompensateParams({
-//             creditPositionWithDebtToRepayId: creditPositionId3,
-//             creditPositionToCompensateId: creditPositionId2_1,
-//             amount: type(uint256).max
-//         })
-//     );
-//     vm.stopPrank();
+        vm.startPrank(alice);
+        vm.expectRevert(abi.encodeWithSelector(Errors.LOAN_NOT_ACTIVE.selector, creditPositionId3));
+        size.compensate(
+            CompensateParams({
+                creditPositionWithDebtToRepayId: creditPositionId3,
+                creditPositionToCompensateId: creditPositionId2_1,
+                amount: type(uint256).max
+            })
+        );
+        vm.stopPrank();
 
-//     uint256 l1 = _borrowAsMarketOrder(bob, alice, 20e6, block.timestamp + 12 days);
-//     uint256 l2 = _borrowAsMarketOrder(alice, james, 20e6, block.timestamp + 6 days);
-//     uint256 creditPositionIdL2 = size.getCreditPositionIdsByDebtPositionId(l2)[0];
-//     uint256 creditPositionIdL1 = size.getCreditPositionIdsByDebtPositionId(l1)[0];
-//     vm.startPrank(alice);
-//     vm.expectRevert(
-//         abi.encodeWithSelector(Errors.DUE_DATE_NOT_COMPATIBLE.selector, creditPositionIdL2, creditPositionIdL1)
-//     );
-//     size.compensate(
-//         CompensateParams({
-//             creditPositionWithDebtToRepayId: creditPositionIdL2,
-//             creditPositionToCompensateId: creditPositionIdL1,
-//             amount: type(uint256).max
-//         })
-//     );
-//     vm.stopPrank();
+        uint256 l1 = _borrow(bob, alice, 20e6, block.timestamp + 12 days);
+        uint256 l2 = _borrow(alice, james, 20e6, block.timestamp + 6 days);
+        uint256 creditPositionIdL2 = size.getCreditPositionIdsByDebtPositionId(l2)[0];
+        uint256 creditPositionIdL1 = size.getCreditPositionIdsByDebtPositionId(l1)[0];
+        vm.startPrank(alice);
+        vm.expectRevert(
+            abi.encodeWithSelector(Errors.DUE_DATE_NOT_COMPATIBLE.selector, creditPositionIdL2, creditPositionIdL1)
+        );
+        size.compensate(
+            CompensateParams({
+                creditPositionWithDebtToRepayId: creditPositionIdL2,
+                creditPositionToCompensateId: creditPositionIdL1,
+                amount: type(uint256).max
+            })
+        );
+        vm.stopPrank();
 
-//     vm.startPrank(bob);
-//     vm.expectRevert(abi.encodeWithSelector(Errors.INVALID_CREDIT_POSITION_ID.selector, loanId2));
-//     size.compensate(
-//         CompensateParams({
-//             creditPositionWithDebtToRepayId: loanId2,
-//             creditPositionToCompensateId: creditPositionId3,
-//             amount: type(uint256).max
-//         })
-//     );
-//     vm.stopPrank();
+        vm.startPrank(bob);
+        vm.expectRevert(abi.encodeWithSelector(Errors.INVALID_CREDIT_POSITION_ID.selector, loanId2));
+        size.compensate(
+            CompensateParams({
+                creditPositionWithDebtToRepayId: loanId2,
+                creditPositionToCompensateId: creditPositionId3,
+                amount: type(uint256).max
+            })
+        );
+        vm.stopPrank();
 
-//     vm.startPrank(bob);
-//     vm.expectRevert(abi.encodeWithSelector(Errors.INVALID_CREDIT_POSITION_ID.selector, debtPositionId));
-//     size.compensate(
-//         CompensateParams({
-//             creditPositionWithDebtToRepayId: creditPositionIdL2,
-//             creditPositionToCompensateId: debtPositionId,
-//             amount: type(uint256).max
-//         })
-//     );
-//     vm.stopPrank();
-// }
+        vm.startPrank(bob);
+        vm.expectRevert(abi.encodeWithSelector(Errors.INVALID_CREDIT_POSITION_ID.selector, debtPositionId));
+        size.compensate(
+            CompensateParams({
+                creditPositionWithDebtToRepayId: creditPositionIdL2,
+                creditPositionToCompensateId: debtPositionId,
+                amount: type(uint256).max
+            })
+        );
+        vm.stopPrank();
+    }
 }
--- test/fixed/LiquidateValidation.t.sol
+++ test/fixed/LiquidateValidation.t.sol
@@ -9,88 +9,88 @@ import {LiquidateParams} from "@src/libraries/fixed/actions/Liquidate.sol";
 import {Errors} from "@src/libraries/Errors.sol";
 
 contract LiquidateValidationTest is BaseTest {
-// function test_Liquidate_validation() public {
-//     _deposit(alice, weth, 100e18);
-//     _deposit(alice, usdc, 100e6 + size.feeConfig().fragmentationFee);
-//     _deposit(bob, weth, 100e18);
-//     _deposit(bob, usdc, 100e6);
-//     _deposit(candy, weth, 100e18);
-//     _deposit(candy, usdc, 100e6);
-//     _deposit(james, weth, 100e18);
-//     _deposit(james, usdc, 100e6);
-//     _lendAsLimitOrder(alice, block.timestamp + 12 days, 0.03e18);
-//     _lendAsLimitOrder(bob, block.timestamp + 12 days, 0.03e18);
-//     _lendAsLimitOrder(candy, block.timestamp + 12 days, 0.03e18);
-//     _lendAsLimitOrder(james, block.timestamp + 12 days, 0.03e18);
-//     _borrowAsMarketOrder(bob, candy, 90e6, block.timestamp + 12 days);
+    function test_Liquidate_validation() public {
+        _deposit(alice, weth, 100e18);
+        _deposit(alice, usdc, 150e6);
+        _deposit(bob, weth, 100e18);
+        _deposit(bob, usdc, 100e6);
+        _deposit(candy, weth, 100e18);
+        _deposit(candy, usdc, 150e6);
+        _deposit(james, weth, 100e18);
+        _deposit(james, usdc, 150e6);
+        _lendAsLimitOrder(alice, block.timestamp + 12 days, 0.03e18);
+        _lendAsLimitOrder(bob, block.timestamp + 12 days, 0.03e18);
+        _lendAsLimitOrder(candy, block.timestamp + 12 days, 0.03e18);
+        _lendAsLimitOrder(james, block.timestamp + 12 days, 0.03e18);
+        _borrow(bob, candy, 90e6, block.timestamp + 12 days);
 
-//     uint256 debtPositionId = _borrowAsMarketOrder(bob, alice, 100e6, block.timestamp + 12 days);
-//     uint256 creditId = size.getCreditPositionIdsByDebtPositionId(debtPositionId)[0];
-//     _borrowAsMarketOrder(alice, james, 20e6, block.timestamp + 12 days, [creditId]);
-//     uint256 creditPositionId = size.getCreditPositionIdsByDebtPositionId(debtPositionId)[1];
-//     uint256 minimumCollateralProfit = 0;
+        uint256 debtPositionId = _borrow(bob, alice, 100e6, block.timestamp + 12 days);
+        uint256 creditId = size.getCreditPositionIdsByDebtPositionId(debtPositionId)[1];
+        _sellCreditMarket(alice, james, creditId, 20e6, block.timestamp + 12 days);
+        uint256 creditPositionId = size.getCreditPositionIdsByDebtPositionId(debtPositionId)[2];
+        uint256 minimumCollateralProfit = 0;
 
-//     _deposit(liquidator, usdc, 10_000e6);
+        _deposit(liquidator, usdc, 10_000e6);
 
-//     vm.startPrank(liquidator);
-//     vm.expectRevert(abi.encodeWithSelector(Errors.INVALID_DEBT_POSITION_ID.selector, creditPositionId));
-//     size.liquidate(
-//         LiquidateParams({debtPositionId: creditPositionId, minimumCollateralProfit: minimumCollateralProfit})
-//     );
-//     vm.stopPrank();
+        vm.startPrank(liquidator);
+        vm.expectRevert(abi.encodeWithSelector(Errors.INVALID_DEBT_POSITION_ID.selector, creditPositionId));
+        size.liquidate(
+            LiquidateParams({debtPositionId: creditPositionId, minimumCollateralProfit: minimumCollateralProfit})
+        );
+        vm.stopPrank();
 
-//     vm.startPrank(liquidator);
-//     vm.expectRevert(
-//         abi.encodeWithSelector(
-//             Errors.LOAN_NOT_LIQUIDATABLE.selector, debtPositionId, size.collateralRatio(bob), LoanStatus.ACTIVE
-//         )
-//     );
-//     size.liquidate(
-//         LiquidateParams({debtPositionId: debtPositionId, minimumCollateralProfit: minimumCollateralProfit})
-//     );
-//     vm.stopPrank();
+        vm.startPrank(liquidator);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Errors.LOAN_NOT_LIQUIDATABLE.selector, debtPositionId, size.collateralRatio(bob), LoanStatus.ACTIVE
+            )
+        );
+        size.liquidate(
+            LiquidateParams({debtPositionId: debtPositionId, minimumCollateralProfit: minimumCollateralProfit})
+        );
+        vm.stopPrank();
 
-//     _borrowAsMarketOrder(alice, candy, 10e6, block.timestamp + 12 days, [creditId]);
-//     _borrowAsMarketOrder(alice, james, 50e6, block.timestamp + 12 days);
+        _sellCreditMarket(alice, candy, creditId, 10e6, block.timestamp + 12 days);
+        _borrow(alice, james, 50e6, block.timestamp + 12 days);
 
-//     // DebtPosition with high CR cannot be liquidated
-//     vm.startPrank(liquidator);
-//     vm.expectRevert(
-//         abi.encodeWithSelector(
-//             Errors.LOAN_NOT_LIQUIDATABLE.selector, debtPositionId, size.collateralRatio(bob), LoanStatus.ACTIVE
-//         )
-//     );
-//     size.liquidate(
-//         LiquidateParams({debtPositionId: debtPositionId, minimumCollateralProfit: minimumCollateralProfit})
-//     );
-//     vm.stopPrank();
+        // DebtPosition with high CR cannot be liquidated
+        vm.startPrank(liquidator);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Errors.LOAN_NOT_LIQUIDATABLE.selector, debtPositionId, size.collateralRatio(bob), LoanStatus.ACTIVE
+            )
+        );
+        size.liquidate(
+            LiquidateParams({debtPositionId: debtPositionId, minimumCollateralProfit: minimumCollateralProfit})
+        );
+        vm.stopPrank();
 
-//     _setPrice(0.01e18);
+        _setPrice(0.01e18);
 
-//     // CreditPosition cannot be liquidated
-//     vm.startPrank(liquidator);
-//     vm.expectRevert(abi.encodeWithSelector(Errors.INVALID_DEBT_POSITION_ID.selector, creditPositionId));
-//     size.liquidate(
-//         LiquidateParams({debtPositionId: creditPositionId, minimumCollateralProfit: minimumCollateralProfit})
-//     );
-//     vm.stopPrank();
+        // CreditPosition cannot be liquidated
+        vm.startPrank(liquidator);
+        vm.expectRevert(abi.encodeWithSelector(Errors.INVALID_DEBT_POSITION_ID.selector, creditPositionId));
+        size.liquidate(
+            LiquidateParams({debtPositionId: creditPositionId, minimumCollateralProfit: minimumCollateralProfit})
+        );
+        vm.stopPrank();
 
-//     _setPrice(100e18);
-//     _repay(bob, debtPositionId);
-//     _withdraw(bob, weth, 98e18);
+        _setPrice(100e18);
+        _repay(bob, debtPositionId);
+        _withdraw(bob, weth, 98e18);
 
-//     _setPrice(0.2e18);
+        _setPrice(0.2e18);
 
-//     // REPAID loan cannot be liquidated
-//     vm.startPrank(liquidator);
-//     vm.expectRevert(
-//         abi.encodeWithSelector(
-//             Errors.LOAN_NOT_LIQUIDATABLE.selector, debtPositionId, size.collateralRatio(bob), LoanStatus.REPAID
-//         )
-//     );
-//     size.liquidate(
-//         LiquidateParams({debtPositionId: debtPositionId, minimumCollateralProfit: minimumCollateralProfit})
-//     );
-//     vm.stopPrank();
-// }
+        // REPAID loan cannot be liquidated
+        vm.startPrank(liquidator);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Errors.LOAN_NOT_LIQUIDATABLE.selector, debtPositionId, size.collateralRatio(bob), LoanStatus.REPAID
+            )
+        );
+        size.liquidate(
+            LiquidateParams({debtPositionId: debtPositionId, minimumCollateralProfit: minimumCollateralProfit})
+        );
+        vm.stopPrank();
+    }
 }
