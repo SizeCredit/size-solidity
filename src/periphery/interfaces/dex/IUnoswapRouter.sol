777117fd40a2ad0f6d2d6bca0acfecaf234858ac
Imtiyaz <imtiyazmiah@gmail.com>
2024-06-05 16:35:37 +0100

    migrate periphery contracts to separate repo

--- src/periphery/interfaces/dex/IUnoswapRouter.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
-
-interface IUnoswapRouter {
-    function unoswapTo(address recipient, address srcToken, uint256 amount, uint256 minReturn, address pool)
-        external
-        payable
-        returns (uint256 returnAmount);
-}
