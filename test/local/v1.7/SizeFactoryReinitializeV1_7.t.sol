6b4db40f2ee42b84d4e4c70be4840a2779624c66
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-02-24 10:14:44 -0300

    Add more tests and README

--- test/local/v1.7/SizeFactoryReinitializeV1_7.t.sol
+++ test/local/v1.7/SizeFactoryReinitializeV1_7.t.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import {SizeFactory} from "@src/factory/SizeFactory.sol";
+import {PAUSER_ROLE} from "@src/factory/interfaces/ISizeFactory.sol";
 import {Errors} from "@src/market/libraries/Errors.sol";
 import {BaseTest} from "@test/BaseTest.sol";
 
@@ -31,5 +35,17 @@ contract SizeFactoryReinitializeV1_7Test is BaseTest {
         assertEq(sizeFactory.owner(), address(0));
         vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, address(this)));
         sizeFactory.reinitialize();
+    }
+
+    function test_SizeFactoryReinitializeV1_7_reinitialize_sizeFactory_only_pauser_can_pause_market() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(IAccessControl.AccessControlUnauthorizedAccount.selector, alice, PAUSER_ROLE)
+        );
+        vm.prank(alice);
+        size.pause();
+
+        AccessControl(address(sizeFactory)).grantRole(PAUSER_ROLE, alice);
+        vm.prank(alice);
+        size.pause();
     }
 }
