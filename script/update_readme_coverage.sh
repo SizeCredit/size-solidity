fa5fe977074a26190bd1b259c06d07982e46aa60
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-02-25 12:25:42 -0300

    Update coverage script

--- script/update_readme_coverage.sh
+++ script/update_readme_coverage.sh
@@ -31,7 +31,9 @@ COVERAGE=$(tail -n +$((COVERAGE_BEGIN)) COVERAGE.txt)
 echo "$PART_1" > README.md
 echo "### FIles" >> README.md
 echo "" >> README.md
+echo '```markdown' >> README.md
 echo "$COVERAGE" >> README.md
+echo '```' >> README.md
 echo "" >> README.md
 echo "### Tests per file" >> README.md
 echo "" >> README.md
