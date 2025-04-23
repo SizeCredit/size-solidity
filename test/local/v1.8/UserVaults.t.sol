238ea291e369636f802fcf37d5085ad8956ff62b
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-04-23 13:14:56 -0300

    Add test_UserVaults_malicious_vault

--- test/local/v1.8/UserVaults.t.sol
+++ test/local/v1.8/UserVaults.t.sol
@@ -145,22 +145,36 @@ contract UserVaultsTest is BaseTest {
         assertEq(usdc.balanceOf(alice), balanceBefore + 99e6, "user should have received only available liquidity");
     }
 
-    function test_UserVaults_malicious_vault() public {}
+    function test_UserVaults_malicious_vault() public {
+        _setUserVaultWhitelistEnabled(false);
+        _setUserConfiguration(alice, address(vaultMalicious), 1.5e18, false, false, new uint256[](0));
+
+        _deposit(alice, usdc, 200e6);
+        _deposit(bob, weth, 100e18);
+        _buyCreditLimit(alice, block.timestamp + 365 days, YieldCurveHelper.pointCurve(365 days, 0.03e18));
+
+        uint256 amount = 100e6;
+        uint256 tenor = 365 days;
+
+        vm.expectRevert(abi.encodeWithSelector(MaliciousERC4626.WithdrawNotAllowed.selector));
+        vm.prank(bob);
+        size.sellCreditMarket(
+            SellCreditMarketParams({
+                lender: alice,
+                creditPositionId: RESERVED_ID,
+                amount: amount,
+                tenor: tenor,
+                deadline: block.timestamp,
+                maxAPR: type(uint256).max,
+                exactAmountIn: false
+            })
+        );
+    }
+
     function test_UserVaults_fee_on_transfer_vault() public {}
-    function test_UserVaults_cannot_choose_vault_with_wrong_underlying() public {}
-    function test_UserVaults_cannot_choose_non_erc4626_contract() public {}
+    function test_UserVaults_vault_with_wrong_underlying() public {}
+    function test_UserVaults_non_erc4626_contract() public {}
     function test_UserVaults_erc7540_contract() public {}
-    function test_UserVaults_cannot_choose_vault_that_fails_on_deposit_or_withdraw() public {}
-    function test_UserVaults_change_vault_after_lending_but_before_repay() public {}
-    function test_UserVaults_multiple_vault_changes_between_repay_and_claim() public {}
-    function test_UserVaults_vault_deposit_rejected_when_global_cap_reached() public {}
-    function test_UserVaults_can_still_repay_when_cap_reached() public {}
-    function test_UserVaults_prevent_ghost_shares_or_fake_balance_transfer() public {}
-    function test_UserVaults_share_balance_drops_correctly_on_withdraw() public {}
-    function test_UserVaults_total_supply_correct_across_multiple_vaults() public {}
-    function test_UserVaults_migration_from_sausdc() public {}
-    function test_UserVaults_sausdc_user_can_claim_after_migration() public {}
-    function test_UserVaults_vault_with_zero_shares() public {}
-    function test_UserVaults_deposit_with_fee_on_transfer_token_has_correct_shares() public {}
-    function test_UserVaults_withdrawal_to_zero_address_reverts() public {}
+    function test_UserVaults_dust_shares_when_changing_vaults() public {}
+    function test_UserVaults_total_supply_across_multiple_vaults() public {}
 }
