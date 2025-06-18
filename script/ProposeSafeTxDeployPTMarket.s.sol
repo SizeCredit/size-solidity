2070fb71f11c6f28705d1b4078f99920668ff8b1
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-06-18 10:08:50 -0300

    Deploy PT-sUSDE-25SEP2025 / USDC

---
+++ script/ProposeSafeTxDeployPTMarket.s.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {Safe} from "@safe-utils/Safe.sol";
+import {BaseScript} from "@script/BaseScript.sol";
+import {Contract, Networks} from "@script/Networks.sol";
+import {ISizeFactory} from "@src/factory/interfaces/ISizeFactory.sol";
+import {DataView} from "@src/market/SizeViewData.sol";
+import {ISize} from "@src/market/interfaces/ISize.sol";
+import {
+    InitializeDataParams,
+    InitializeFeeConfigParams,
+    InitializeOracleParams,
+    InitializeRiskConfigParams
+} from "@src/market/libraries/actions/Initialize.sol";
+
+import {IPriceFeed} from "@src/oracle/IPriceFeed.sol";
+import {IMorphoChainlinkOracleV2} from "@src/oracle/adapters/morpho/IMorphoChainlinkOracleV2.sol";
+import {PriceFeedMorphoChainlinkOracleV2} from "@src/oracle/v1.7.1/PriceFeedMorphoChainlinkOracleV2.sol";
+import {Tenderly} from "@tenderly-utils/Tenderly.sol";
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+import {PendleChainlinkOracle} from "@pendle/contracts/oracles/PtYtLpOracle/chainlink/PendleChainlinkOracle.sol";
+import {PendleSparkLinearDiscountOracle} from "@pendle/contracts/oracles/internal/PendleSparkLinearDiscountOracle.sol";
+import {PriceFeedPendleSparkLinearDiscountChainlink} from
+    "@src/oracle/v1.7.1/PriceFeedPendleSparkLinearDiscountChainlink.sol";
+import {PriceFeedPendleTWAPChainlink} from "@src/oracle/v1.7.2/PriceFeedPendleTWAPChainlink.sol";
+
+import {console} from "forge-std/console.sol";
+
+contract ProposeSafeTxDeployPTMarketScript is BaseScript, Networks {
+    using Tenderly for *;
+    using Safe for *;
+
+    address signer;
+    string derivationPath;
+
+    ISizeFactory private sizeFactory;
+    address private safeAddress;
+
+    IERC20Metadata private underlyingCollateralToken;
+    IPriceFeed private priceFeed;
+
+    modifier parseEnv() {
+        signer = vm.envAddress("SIGNER");
+        derivationPath = vm.envString("LEDGER_PATH");
+        sizeFactory = ISizeFactory(contracts[block.chainid][Contract.SIZE_FACTORY]);
+
+        string memory accountSlug = vm.envString("TENDERLY_ACCOUNT_NAME");
+        string memory projectSlug = vm.envString("TENDERLY_PROJECT_NAME");
+        string memory accessKey = vm.envString("TENDERLY_ACCESS_KEY");
+
+        tenderly.initialize(accountSlug, projectSlug, accessKey);
+
+        safeAddress = vm.envAddress("OWNER");
+        safe.initialize(safeAddress);
+
+        priceFeed = IPriceFeed(vm.envAddress("PRICE_FEED"));
+        underlyingCollateralToken = IERC20Metadata(vm.envAddress("UNDERLYING_COLLATERAL_TOKEN"));
+
+        _;
+    }
+
+    function run() external parseEnv deleteVirtualTestnets {
+        ISize market = sizeFactory.getMarket(1);
+        InitializeFeeConfigParams memory feeConfigParams = market.feeConfig();
+
+        InitializeRiskConfigParams memory riskConfigParams = market.riskConfig();
+
+        InitializeOracleParams memory oracleParams = market.oracle(); // priceFeed replaced below
+        oracleParams.priceFeed = address(priceFeed);
+
+        DataView memory dataView = market.data();
+        InitializeDataParams memory dataParams = InitializeDataParams({
+            weth: contracts[block.chainid][Contract.WETH],
+            underlyingCollateralToken: address(underlyingCollateralToken),
+            underlyingBorrowToken: address(dataView.underlyingBorrowToken),
+            variablePool: address(dataView.variablePool),
+            borrowATokenV1_5: address(dataView.borrowAToken),
+            sizeFactory: address(sizeFactory)
+        });
+        bytes memory data = abi.encodeCall(ISizeFactory.createMarket, (feeConfigParams, riskConfigParams, oracleParams, dataParams));
+        address target = address(sizeFactory);
+        safe.proposeTransaction(target, data, signer, derivationPath);
+        Tenderly.VirtualTestnet memory vnet = tenderly.createVirtualTestnet("pt-market-vnet", block.chainid);
+        bytes memory execTransactionData = safe.getExecTransactionData(target, data, signer, derivationPath);
+        tenderly.setStorageAt(vnet, safe.instance().safe, bytes32(uint256(4)), bytes32(uint256(1)));
+        tenderly.sendTransaction(vnet.id, signer, safe.instance().safe, execTransactionData);
+    }
+}
