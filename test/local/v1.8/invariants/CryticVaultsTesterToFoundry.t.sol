4bb0a1be9873f2f1fb3efae9b97690fc010555c1
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-06-19 10:26:44 -0300

    Add reentrancy tests

--- test/local/v1.8/invariants/CryticVaultsTesterToFoundry.t.sol
+++ test/local/v1.8/invariants/CryticVaultsTesterToFoundry.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import {VaultsTargetFunctions} from "@test/local/v1.8/invariants/VaultsTargetFunctions.sol";
 
 import {Asserts} from "@chimera/Asserts.sol";
@@ -30,5 +31,34 @@ contract CryticVaultsTesterToFoundry is BaseTest, VaultsTargetFunctions, Foundry
 
     modifier getSender() override {
         _;
+    }
+
+    function test_CryticVaultsTesterToFoundry_1() public {
+        maliciousVault_setSize(false);
+        token_approve(1);
+        borrowTokenVault_setVault(address(0xdeadbeef), address(0x16), false);
+        size_deposit(1, address(0x30000));
+        maliciousVault_setOperation(0x0000003e);
+        maliciousVault_setReenterCount(1);
+        vm.expectRevert(ReentrancyGuardUpgradeable.ReentrancyGuardReentrantCall.selector);
+        borrowTokenVault_setVault(address(0xdeadbeef), address(0x0), false);
+    }
+
+    function test_CryticVaultsTesterToFoundry_2() public {
+        sender = 0x0000000000000000000000000000000000020000;
+        borrowTokenVault_setVault(address(0xdeadbeef), address(0x16), false);
+        sender = 0x0000000000000000000000000000000000030000;
+        maliciousVault_setSize(true);
+        sender = 0x0000000000000000000000000000000000030000;
+        token_approve(1524785991);
+        sender = 0x0000000000000000000000000000000000030000;
+        size_deposit(4370000, address(0x30000));
+        sender = 0x0000000000000000000000000000000000010000;
+        maliciousVault_setOperation(0x713fc232);
+        sender = 0x0000000000000000000000000000000000010000;
+        maliciousVault_setReenterCount(81);
+        sender = 0x0000000000000000000000000000000000030000;
+        vm.expectRevert(ReentrancyGuardUpgradeable.ReentrancyGuardReentrantCall.selector);
+        size_withdraw(68625291814337064871416729962860020715493080276270229296212733239041971906714, address(0x20000));
     }
 }
