b37a31264e6b170cc74baa98039742fa5377e43c
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-07-15 10:17:52 +0200

    Update echidna/medusa to crytic-compile 0.3.10

--- test/invariants/crytic/CryticTester.sol
+++ test/invariants/crytic/CryticTester.sol
@@ -6,7 +6,7 @@ import {CryticAsserts} from "@chimera/CryticAsserts.sol";
 import {SetupLocal} from "@test/invariants/SetupLocal.sol";
 import {TargetFunctions} from "@test/invariants/TargetFunctions.sol";
 
-// echidna . --contract CryticTester --config echidna.yaml
+// echidna test/invariants/crytic/CryticTester.sol --contract CryticTester --config echidna.yaml
 // medusa fuzz
 contract CryticTester is TargetFunctions, SetupLocal, CryticAsserts {
     constructor() {
